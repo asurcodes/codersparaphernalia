a976978d2f0739d99246389dbb00ee47cd28c1a5
asurbernardo <asur@asurbernardo.com>
2019-01-25 21:46:40 +0100

    Add image rigths disclamer to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,13 @@
 <footer>
     <div class="container">
-        <p>Made by Asur Bernardo</p>
-        <p>with &#10084; and <b>no design skills</b></p>
+        <p>Made with &#10084; and <b>no design skills</b></p>
+        <p>
+            Image rights belongs to
+            <a rel="nofollow" href="https://amazon.com">Amazon</a>,
+            <a rel="nofollow" href="https://redbubble.com">RedBubble</a>,
+            <a rel="nofollow" href="https://etsy.com">Etsy</a>,
+            <a rel="nofollow" href="https://udemy.com">Udemy</a> and
+            <a rel="nofollow" href="https://g2a.com">G2A</a>
+        </p>
     </div>
 </footer>
