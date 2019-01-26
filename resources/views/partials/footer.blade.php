5fe4142df561701cb64b52108ced9f36ede4a39d
asurbernardo <asur@asurbernardo.com>
2019-01-26 21:58:48 +0100

    Add social share buttons

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,12 @@
 <footer>
     <div class="container">
+        <p>Do you like what you see? Share it!</p>
+        <amp-social-share type="twitter" aria-label="Share this on Twitter"></amp-social-share>
+        <amp-social-share type="facebook" aria-label="Share this on Facebook"></amp-social-share>
+        <amp-social-share type="gplus" aria-label="Share this on Google Plus"></amp-social-share>
+        <amp-social-share type="pinterest" aria-label="Share this on Pinterest"></amp-social-share>
+    </div>
+    <div class="container">
         <p>Made with &#10084; and <b>no design skills</b></p>
         <p>
             Image rights belongs to
