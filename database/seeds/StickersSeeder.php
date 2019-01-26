62075aa0771b1c34070abbd1845a03b2fce29d0c
asurbernardo <asur@asurbernardo.com>
2019-01-26 19:14:13 +0100

    Add more seeded products to be able to test pagination

--- database/seeds/StickersSeeder.php
+++ database/seeds/StickersSeeder.php
@@ -86,6 +86,114 @@ class StickersSeeder extends Seeder
                 'tags' => ['foo'],
                 'vendor' => 'redbubble'
             ],
+            [
+                'slug' => 'product-nine',
+                'title' => 'Product nine',
+                'image' => 'https://placehold.it/600x600',
+                'url' => 'http://example.com',
+                'price' => 81.4,
+                'tags' => ['foo'],
+                'vendor' => 'redbubble'
+            ],
+            [
+                'slug' => 'product-ten',
+                'title' => 'Product ten',
+                'image' => 'https://placehold.it/600x600',
+                'url' => 'http://example.com',
+                'price' => 81.4,
+                'tags' => ['foo'],
+                'vendor' => 'redbubble'
+            ],
+            [
+                'slug' => 'product-eleven',
+                'title' => 'Product eleven',
+                'image' => 'https://placehold.it/600x600',
+                'url' => 'http://example.com',
+                'price' => 81.4,
+                'tags' => ['foo'],
+                'vendor' => 'redbubble'
+            ],
+            [
+                'slug' => 'product-twelve',
+                'title' => 'Product twelve',
+                'image' => 'https://placehold.it/600x600',
+                'url' => 'http://example.com',
+                'price' => 81.4,
+                'tags' => ['foo'],
+                'vendor' => 'redbubble'
+            ],
+            [
+                'slug' => 'product-thirteen',
+                'title' => 'Product thirteen',
+                'image' => 'https://placehold.it/600x600',
+                'url' => 'http://example.com',
+                'price' => 81.4,
+                'tags' => ['foo'],
+                'vendor' => 'redbubble'
+            ],
+            [
+                'slug' => 'product-fourteen',
+                'title' => 'Product fourteen',
+                'image' => 'https://placehold.it/600x600',
+                'url' => 'http://example.com',
+                'price' => 81.4,
+                'tags' => ['foo'],
+                'vendor' => 'redbubble'
+            ],
+            [
+                'slug' => 'product-fifteen',
+                'title' => 'Product fifteen',
+                'image' => 'https://placehold.it/600x600',
+                'url' => 'http://example.com',
+                'price' => 81.4,
+                'tags' => ['foo'],
+                'vendor' => 'redbubble'
+            ],
+            [
+                'slug' => 'product-sixteen',
+                'title' => 'Product sixteen',
+                'image' => 'https://placehold.it/600x600',
+                'url' => 'http://example.com',
+                'price' => 81.4,
+                'tags' => ['foo'],
+                'vendor' => 'redbubble'
+            ],
+            [
+                'slug' => 'product-seventeen',
+                'title' => 'Product seventeen',
+                'image' => 'https://placehold.it/600x600',
+                'url' => 'http://example.com',
+                'price' => 81.4,
+                'tags' => ['foo'],
+                'vendor' => 'redbubble'
+            ],
+            [
+                'slug' => 'product-eighteen',
+                'title' => 'Product eighteen',
+                'image' => 'https://placehold.it/600x600',
+                'url' => 'http://example.com',
+                'price' => 81.4,
+                'tags' => ['foo'],
+                'vendor' => 'redbubble'
+            ],
+            [
+                'slug' => 'product-nineteen',
+                'title' => 'Product nineteen',
+                'image' => 'https://placehold.it/600x600',
+                'url' => 'http://example.com',
+                'price' => 81.4,
+                'tags' => ['foo'],
+                'vendor' => 'redbubble'
+            ],
+            [
+                'slug' => 'product-twenty',
+                'title' => 'Product twenty',
+                'image' => 'https://placehold.it/600x600',
+                'url' => 'http://example.com',
+                'price' => 81.4,
+                'tags' => ['foo'],
+                'vendor' => 'redbubble'
+            ]
         ]);
     }
 }
