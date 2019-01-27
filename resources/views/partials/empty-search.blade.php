f2567108d27b3f91b6a2804eb021dfd8447bc719
asurbernardo <asur@asurbernardo.com>
2019-01-27 13:40:31 +0100

    Get the query directly from the request object isntead from the view on the search fallback partial

--- resources/views/partials/empty-search.blade.php
+++ resources/views/partials/empty-search.blade.php
@@ -1,4 +1,4 @@
 <div class="container">
-    <h2> We didn't find any product for the search: {{$query}}</h2>
+    <h2> We didn't find any product for the search: {{request()->get('query')}}</h2>
     <h3>Go back to the <a href="{{route('home')}}">homepage</a> and keep browsing</h3>
 </div>
