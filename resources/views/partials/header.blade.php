6713386cc6475c400f2ab8939e0e07005b1a273d
asurbernardo <asur@asurbernardo.com>
2019-01-27 13:38:31 +0100

    Change search field name to integrate correctly with Laravel pagination

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <h1>{!!$title!!}</h1>
         <form id="search" method="get" action="{{route('product.search')}}" target="_top">
-            <input name="q" type="input" placeholder="Search for what you want" aria-label="Search for products">
+            <input name="query" type="input" placeholder="Search for what you want" aria-label="Search for products">
             <input type="submit" value="SEARCH">
         </form>
         @if(isset($description))
