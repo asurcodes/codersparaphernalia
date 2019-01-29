e71eeddcee8a92a3d7e75b38f5cc3fa2fbc18c97
asurbernardo <asur@asurbernardo.com>
2019-01-29 20:06:50 +0100

    Change image aspect ratio depending on vendor

--- resources/views/partials/product-card.blade.php
+++ resources/views/partials/product-card.blade.php
@@ -4,11 +4,29 @@
         rel="nofollow"
         target="_blank"
         aria-label="Buy {{$product->title}}">
-            <amp-img src="{{$product->image}}"
-                width="420"
-                height="460"
-                layout="responsive"
-                alt="{{$product->title}}">
+            @switch($product->vendor)
+                @case('g2a')
+                    <amp-img src="{{$product->image}}"
+                        width="350"
+                        height="466"
+                        layout="responsive"
+                        alt="{{$product->title}}">
+                    @break
+                @case('udemy')
+                    <amp-img src="{{$product->image}}"
+                        width="480"
+                        height="270"
+                        layout="responsive"
+                        alt="{{$product->title}}">
+                    @break
+                @default
+                    <amp-img src="{{$product->image}}"
+                        width="420"
+                        height="460"
+                        layout="responsive"
+                        alt="{{$product->title}}">
+                    @break
+            @endswitch
     </a>
     <div class="card__price"><b>${{$product->price}}</b></div>
     <div class="card__content">
