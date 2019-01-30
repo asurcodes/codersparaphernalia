a53abbc256fe19ca619c8c0c1300636cfa2e57e8
asurbernardo <asur@asurbernardo.com>
2019-01-30 12:57:32 +0100

    Curate the list of top products selecting them by slug

--- app/Http/View/Composers/HomeComposer.php
+++ app/Http/View/Composers/HomeComposer.php
@@ -15,7 +15,17 @@ class HomeComposer
             return Category::get();
         });
         $topProducts = Cache::remember('top-products', 720, function () {
-            return Product::take(8)->get();
+            $topProductsSlugs = [
+                'gopher-programming-sticker',
+                'yellow-rubber-bath-duck',
+                'anker-wireless-vertical-mouse',
+                'programming-interviews-exposed-book',
+                'code-mandala-react-tote-bag',
+                'dell-xps-9570-laptop',
+                'monoprice-select-mini-pro-3d-printer',
+                'code-debugger-hoodie',
+            ];
+            return Product::whereIn('slug', $topProductsSlugs)->get();
         });
         $view->with('categories', $categories);
         $view->with('topProducts', $topProducts);
