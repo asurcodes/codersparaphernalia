9a70e4202bfb8ea6aa7cf11ecbe440504598deec
asurbernardo <asur@asurbernardo.com>
2019-01-31 20:46:37 +0100

    Add new courses to seeder

--- database/seeds/CoursesSeeder.php
+++ database/seeds/CoursesSeeder.php
@@ -50,6 +50,42 @@ class CoursesSeeder extends Seeder
                 'tags' => ['mysql'],
                 'vendor' => 'udemy'
             ],
+            [
+                'slug' => 'go-the-complete-developers-guide-course',
+                'title' => 'Go: The Complete Developer\'s Guide (Golang)',
+                'image' => 'https://udemy-images.udemy.com/course/480x270/1309202_1231_4.jpg',
+                'url' => 'https://www.udemy.com/go-the-complete-developers-guide/',
+                'price' => 9.99,
+                'tags' => [],
+                'vendor' => 'udemy'
+            ],
+            [
+                'slug' => 'graphql-with-react-course',
+                'title' => 'GraphQL with React: The Complete Developers Guide',
+                'image' => 'https://udemy-images.udemy.com/course/480x270/1109926_7f97_2.jpg',
+                'url' => 'https://www.udemy.com/graphql-with-react-course/',
+                'price' => 9.99,
+                'tags' => [],
+                'vendor' => 'udemy'
+            ],
+            [
+                'slug' => 'nodejs-the-complete-guide-course',
+                'title' => 'NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)',
+                'image' => 'https://udemy-images.udemy.com/course/480x270/1879018_95b6.jpg',
+                'url' => 'https://www.udemy.com/nodejs-the-complete-guide/',
+                'price' => 9.99,
+                'tags' => [],
+                'vendor' => 'udemy'
+            ],
+            [
+                'slug' => 'ios-12-app-development-bootcamp-course',
+                'title' => 'iOS 12 & Swift - The Complete iOS App Development Bootcamp',
+                'image' => 'https://udemy-images.udemy.com/course/480x270/1778502_f4b9_8.jpg',
+                'url' => 'https://www.udemy.com/ios-12-app-development-bootcamp/',
+                'price' => 9.99,
+                'tags' => [],
+                'vendor' => 'udemy'
+            ],
         ]);
     }
 }
