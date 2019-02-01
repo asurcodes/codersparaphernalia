bfd58a174c486f9242c88c7a3019fa7fb2dcd004
asurbernardo <asur@asurbernardo.com>
2019-02-01 17:19:20 +0100

    Fix broken link on System76 laptop

--- database/seeds/ComputersSeeder.php
+++ database/seeds/ComputersSeeder.php
@@ -126,7 +126,7 @@ class ComputersSeeder extends Seeder
                 'slug' => 'system-76-gazelle-laptop',
                 'title' => 'System76 Gazelle, Fully Configurable Windows Free Laptop',
                 'image' => 'https://d1vhcvzji58n1j.cloudfront.net/assets/products/gaze13/thumb-1e9786a660.png',
-                'url' => 'https://system76.com/cart/configure/gaze13r',
+                'url' => 'https://system76.com/laptops/gazelle',
                 'price' => 869.00,
                 'tags' => [],
                 'vendor' => 'amazon'
