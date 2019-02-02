c409f45be7baa75fa63697eb006f6e2656692aa2
asurbernardo <asur@asurbernardo.com>
2019-02-03 00:06:52 +0100

    Add default timestamp value to categories

--- database/migrations/2019_01_21_144151_create_categories_table.php
+++ database/migrations/2019_01_21_144151_create_categories_table.php
@@ -23,7 +23,7 @@ class CreateCategoriesTable extends Migration
             $table->text('meta_description');
             $table->longText('long_description');
             $table->json('related');
-            $table->timestamps();
+            $table->timestamps()->useCurrent();;
         });
     }
 
