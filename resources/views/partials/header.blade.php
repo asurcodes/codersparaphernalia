44c1d1796d0c809fca458b8f99af393b883532d3
asurbernardo <asur@asurbernardo.com>
2019-06-16 19:03:20 +0200

    Restructure the header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,22 @@
 <header>
     <div class="container">
-        <h1>{!!$title!!}</h1>
+        <a href="{{route('home')}}">
+            <amp-img src="{{url('images/header.png')}}"
+                width="1960"
+                height="430"
+                layout="responsive"
+                alt="{{$title}}">
+            </amp-img>
+        </a>
+    </div>
+    <div class="container">
         <form id="search" method="get" action="{{route('product.search')}}" target="_top">
             <input name="query" type="input" placeholder="Search for what you want" aria-label="Search for products">
             <input type="submit" value="SEARCH">
         </form>
+    </div>
+    <div class="container">
+        <h1>{!!$title!!}</h1>
         @if(isset($description))
             {!!$description!!}
         @endif
