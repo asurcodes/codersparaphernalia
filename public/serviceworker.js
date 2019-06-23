dc16d04a438b7f3cc3dd771667542a82a16d955d
asurbernardo <asur@asurbernardo.com>
2019-06-23 14:55:38 +0200

    Improve service worker configuration

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,7 +1,12 @@
 importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
 AMP_SW.init({
+    assetCachingOptions: [{
+    regexp: /\.(png|jpg|woff2|woff|css|js)/,
+    cachingStrategy: 'NETWORK_FIRST',
+  }],
   offlinePageOptions: {
     url: '/offline',
     assets: [],
-  }
+  },
+  linkPrefetchOptions: {}
 });
