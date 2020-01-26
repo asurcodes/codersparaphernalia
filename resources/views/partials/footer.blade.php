d44b21d4026fee862c42f6369d516c7f89ae9635
Asur <asur.bernardo@the-cocktail.com>
2020-01-26 20:15:33 +0100

    Update footer feel

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,15 +1,15 @@
 <footer>
     <div class="container">
-        <p>Do you like what you see? Share it!</p>
+        <p>Dis you like our listings? Share them!</p>
         <amp-social-share type="twitter" aria-label="Share this on Twitter"></amp-social-share>
         <amp-social-share type="facebook" aria-label="Share this on Facebook"></amp-social-share>
         <amp-social-share type="gplus" aria-label="Share this on Google Plus"></amp-social-share>
         <amp-social-share type="pinterest" aria-label="Share this on Pinterest"></amp-social-share>
     </div>
     <div class="container">
-        <p>Made with &#10084; and <b>no design skills</b></p>
+        <p>Made with &#10084; and <b>no design skills</b> by <a href="https://asur.dev/en">asur.dev</a></p>
         <p>
-            © CodersParaphernalia {{date("Y")}} - Image rights belong to
+            © CodersParaphernalia {{date("Y")}} - Products image rights belong to
             <a rel="nofollow" href="https://amazon.com">Amazon</a>,
             <a rel="nofollow" href="https://redbubble.com">RedBubble</a>,
             <a rel="nofollow" href="https://etsy.com">Etsy</a>,
